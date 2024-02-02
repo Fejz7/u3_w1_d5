bde39e56ac0c1f4d6693eb76ecddf797234d6051
Fejz7 <ZOAC@outlook.it>
2024-02-02 16:41:53 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import MainMovie from './components/MainMovie';
+import SecondMovie from './components/SecondMovie';
+import ThirdMovie from './components/ThirdMovie';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>
+        <Navbar/>
+         </header>
+      <main>
+        <MainMovie/>
+      <SecondMovie/>
+      <ThirdMovie/>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </>
   );
 }
 
